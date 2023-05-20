aa2bbcfc849b0506eb0fc2bc3416dfb6a7168529
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2023-05-21 00:19:18 +0200

    Remove all except matchbox_server

--- matchbox_socket/src/error.rs
+++
@@ -1,9 +0,0 @@
-use crate::webrtc_socket::error::SignalingError;
-
-/// Errors that can happen when using Matchbox.
-#[derive(Debug, thiserror::Error)]
-pub enum Error {
-    /// An error occurring during the signaling loop.
-    #[error("An error in the signaling loop")]
-    Signaling(#[from] SignalingError),
-}
